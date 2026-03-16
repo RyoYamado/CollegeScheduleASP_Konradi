dedeafb8a3e7e7d557547a575dd882da72ba4748
D1ff <plagueissimple@gmail.com>
2026-03-16 17:09:41 +0300

    Добавлены новые пакеты и классы для работы с расписанием
    В проекте `CollegeSchedule_Konradi.csproj` добавлены ссылки на пакеты `DotNetEnv`, `Microsoft.EntityFrameworkCore.Design` и `Npgsql.EntityFrameworkCore.PostgreSQL`, а также новая папка `Middlewares`.
    
    Изменена структура класса `AppDbContext`, добавлены свойства для работы с сущностями и переопределен метод `OnModelCreating`.
    
    Добавлены атрибуты и свойства в классах `Building`, `Classroom`, `LessonGroupPart`, `LessonTime`, `Schedule`, `Specialty`, `StudentGroup`, `Subject`, `Teacher` и `Weekday` для работы с базой данных.
    
    В файле `Program.cs` настроено подключение к базе данных с использованием `Npgsql`.
    
    Добавлены новые классы и интерфейсы для работы с расписанием, включая `LessonDto`, `LessonPartDto`, `ScheduleByDateDto` и интерфейс `IScheduleService`.
    
    Обновлены зависимости в файле `project.assets.json`, удалены устаревшие зависимости, что улучшает совместимость с новыми версиями .NET.

--- Models/LessonGroupPart.cs
+++ Models/LessonGroupPart.cs
@@ -1,6 +1,9 @@
-﻿namespace CollegeSchedule_Konradi.Models
+﻿namespace CollegeSchedule.Models
 {
-    public class LessonGroupPart
+    public enum LessonGroupPart
     {
+        FULL = 0,
+        SUB1 = 1,
+        SUB2 = 2
     }
 }
