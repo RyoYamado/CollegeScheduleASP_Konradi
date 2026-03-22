d2b4d64e6a057de4e5bd0c84a0f1fe1ccbf3b697
D1ff <plagueissimple@gmail.com>
2026-03-22 12:15:44 +0300

    Обновлен ScheduleController

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -3,27 +3,82 @@ using CollegeSchedule.Models;
 using CollegeSchedule.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 namespace CollegeSchedule.Controllers
 {
     [ApiController]
-    [Route("api/schedule")]
+    [Route("api/[controller]")]
     public class ScheduleController : ControllerBase
     {
         private readonly IScheduleService _service;
-        public ScheduleController(IScheduleService service, AppDbContext db)
+
+        public ScheduleController(IScheduleService service)
         {
             _service = service;
         }
         
         // GET: api/schedule/group/{groupName}?start=2026-01-12&end=2026-01-17
         [HttpGet("group/{groupName}")]
-        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        public async Task<IActionResult> GetScheduleByGroup(string groupName, [FromQuery] DateTime start, [FromQuery] DateTime end)
         {
-            // Вызываем бизнес-логику из сервиса
-                var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
-                // Возвращаем результат со статусом 200 OK
-                return Ok(result);
+            var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
+            return Ok(result);
         }
 
+        // GET: api/schedule/teacher/{teacherId}?start=2026-01-12&end=2026-01-17
+        [HttpGet("teacher/{teacherId}")]
+        public async Task<IActionResult> GetScheduleByTeacher(int teacherId, [FromQuery] DateTime start, [FromQuery] DateTime end)
+        {
+            var result = await _service.GetScheduleForTeacher(teacherId, start.Date, end.Date);
+            return Ok(result);
+        }
+
+        // GET: api/schedule/classroom/{classroomId}?start=2026-01-12&end=2026-01-17
+        [HttpGet("classroom/{classroomId}")]
+        public async Task<IActionResult> GetScheduleByClassroom(int classroomId, [FromQuery] DateTime start, [FromQuery] DateTime end)
+        {
+            var result = await _service.GetScheduleForClassroom(classroomId, start.Date, end.Date);
+            return Ok(result);
+        }
+
+        // GET: api/schedule/teachers
+        [HttpGet("teachers")]
+        public async Task<IActionResult> GetAllTeachers()
+        {
+            var result = await _service.GetAllTeachers();
+            return Ok(result);
+        }
+
+        // GET: api/schedule/groups
+        [HttpGet("groups")]
+        public async Task<IActionResult> GetAllGroups()
+        {
+            var result = await _service.GetAllGroups();
+            return Ok(result);
+        }
+
+        // GET: api/schedule/classrooms
+        [HttpGet("classrooms")]
+        public async Task<IActionResult> GetAllClassrooms()
+        {
+            var result = await _service.GetAllClassrooms();
+            return Ok(result);
+        }
+
+        // GET: api/schedule/subjects
+        [HttpGet("subjects")]
+        public async Task<IActionResult> GetAllSubjects()
+        {
+            var result = await _service.GetAllSubjects();
+            return Ok(result);
+        }
+
+        // GET: api/schedule/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetScheduleById(int id)
+        {
+            var result = await _service.GetScheduleById(id);
+            return Ok(result);
+        }
     }
 }
